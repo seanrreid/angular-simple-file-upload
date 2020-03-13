468745d0397c6a51402c4a83a09ad0d6333083d4
Sean R Reid <sean@torchcodelab.com>
2020-03-13 14:37:17 -0400

    Added a progress bar and HttpEvent types

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
 import { ImageUploadService } from "./image-upload.service";
 
-class FileSnippet {
-  constructor(public src: string, public file: File) {}
+interface IFileInput {
+  readonly imageInput: HTMLInputElement;
+  files: Array<File>;
 }
 
 @Component({
@@ -11,20 +12,52 @@ class FileSnippet {
   styleUrls: ["./file-upload.component.scss"]
 })
 export class FileUploadComponent {
-  selectedFile: FileSnippet;
-  fileName: string;
+  selectedFile: boolean = false;
+  fileUploaded: boolean = false;
+  fileExists: boolean = false;
+  uploadError: boolean = false;
+  fileName: string = "";
+  base64File: string[] = [];
+  progress: number = 0;
 
-  constructor(private imageService: ImageUploadService) {}
+  constructor(private readonly imageUploadService: ImageUploadService) {}
 
-  processFile(imageInput: any) {
-    const file: File = imageInput.files[0];
-    const reader = new FileReader();
-    this.fileName = file.name;
+  processFile(imageInput: IFileInput): void {
+    try {
+      const file: File = imageInput.files[0];
+      const reader = new FileReader();
+      this.fileName = file.name;
 
-    reader.addEventListener("load", (event: any) => {
-      this.selectedFile = new FileSnippet(event.target.result, file);
-    });
-
-    reader.readAsDataURL(file);
+      reader.readAsDataURL(file);
+      reader.onload = (): void => {
+        if (typeof reader.result === "string") {
+          this.selectedFile = true;
+          this.base64File = reader.result.split(",");
+        }
+        const fileExtension = file.name.split(".").pop();
+        this.imageUploadService
+          .uploadImage(this.base64File[1], fileExtension)
+          .subscribe(
+            (res: { status: string; code: number }) => {
+              console.log("res ", res);
+              const { status, code } = res;
+              if (status === "progress") {
+                this.progress = code;
+              }
+              if (status === "OK" || status === "pre_process") {
+                this.fileUploaded = true;
+                this.uploadError = false;
+              }
+              return status;
+            },
+            (error: Error) => {
+              this.uploadError = true;
+              return error;
+            }
+          );
+      };
+    } catch (error) {
+      this.uploadError = true;
+    }
   }
 }
--- src/app/file-upload/image-upload.service.ts
+++ src/app/file-upload/image-upload.service.ts
@@ -1,20 +1,60 @@
-import { map } from "rxjs/operators";
 import { Injectable } from "@angular/core";
-import { Response } from "@angular/http";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient, HttpEventType } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
+
+interface IUploadImageResponse {
+  status: string;
+  code: number;
+}
 
 @Injectable()
 export class ImageUploadService {
   constructor(private http: HttpClient) {}
 
-  public uploadImage(image: File): Observable<string | any> {
-    const formData = new FormData();
+  public uploadImage(
+    image: string,
+    type?: string
+  ): Observable<string | { status: string; code: number }> {
+    const docUpload = {
+      DocumentType: "paystub",
+      Pages: [{ Data: image, Extension: type }]
+    };
 
-    formData.append("image", image);
+    const url = `http://localhost:3000`;
 
     return this.http
-      .post("/api/v1/image-upload", formData)
-      .pipe(map((json: any) => json.imageUrl));
+      .post<IUploadImageResponse>(url, docUpload, {
+        reportProgress: true,
+        observe: "events"
+      })
+      .pipe(
+        map(event => {
+          switch (event.type) {
+            case HttpEventType.UploadProgress:
+              const progress = Math.round((100 * event.loaded) / event.total);
+              return { status: "progress", code: progress };
+            case HttpEventType.ResponseHeader:
+              return { status: event.statusText, code: event.status };
+            case HttpEventType.Response:
+              return event.body;
+            case HttpEventType.DownloadProgress:
+              return { status: "download progress", code: event.loaded };
+            default:
+              return { status: "default status", code: null };
+          }
+        })
+      );
   }
 }
+
+//(res: { status: string }) => res.status
+
+/*
+  .pipe(
+        map((res: { status: string }) => res.status),
+        catchError((error: Error) => {
+          return throwError(error);
+        })
+      );
+*/
