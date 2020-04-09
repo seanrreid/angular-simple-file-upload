873583c6700d3807fbabe28dba29cdddffb2ae46
Sean R Reid <sean@torchcodelab.com>
2020-04-09 17:55:38 -0400

    * Changed input to be invisible, and not triggered by the button * Added a new uploadFile method with status returned to the component

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,30 +1,99 @@
 import { Component } from "@angular/core";
+import { HttpClient, HttpEventType, HttpResponse } from "@angular/common/http";
 import { ImageUploadService } from "./image-upload.service";
 
-class FileSnippet {
-  constructor(public src: string, public file: File) {}
+interface IFileInput {
+  readonly imageInput: HTMLInputElement;
+  files: Array<File>;
 }
 
 @Component({
   selector: "app-file-upload",
   templateUrl: "./file-upload.component.html",
-  styleUrls: ["./file-upload.component.scss"]
+  styleUrls: ["./file-upload.component.scss"],
 })
 export class FileUploadComponent {
-  selectedFile: FileSnippet;
-  fileName: string;
+  selectedFile: boolean = false;
+  isDisabled: boolean = true;
+  fileExists: boolean = false;
+  fileUploaded: boolean = false;
+  isLoading: boolean = false;
+  uploadError: boolean = false;
+  fileName: string = "";
+  base64File: string[] = [];
+  percentDone: number;
 
-  constructor(private imageService: ImageUploadService) {}
+  constructor(
+    private readonly imageUploadService: ImageUploadService,
+    private readonly http: HttpClient
+  ) {}
 
-  processFile(imageInput: any) {
-    const file: File = imageInput.files[0];
-    const reader = new FileReader();
-    this.fileName = file.name;
+  processFile(imageInput: IFileInput): void {
+    console.log("Processing file input");
+    try {
+      const file: File = imageInput.files[0];
+      const reader = new FileReader();
+      this.fileName = file.name;
 
-    reader.addEventListener("load", (event: any) => {
-      this.selectedFile = new FileSnippet(event.target.result, file);
-    });
+      reader.readAsDataURL(file);
+      reader.onload = (): void => {
+        if (typeof reader.result === "string") {
+          this.selectedFile = true;
+          this.isDisabled = false;
+          this.isLoading = true;
+          this.base64File = reader.result.split(",");
+        }
+        const fileExtension = file.name.split(".").pop();
 
-    reader.readAsDataURL(file);
+        const url = `https://torchcodelab.free.beeceptor.com`;
+
+        this.imageUploadService
+          .uploadFile(this.base64File[1], fileExtension)
+          .subscribe(
+            (res: { status: string; percent?: number; message?: string }) => {
+              console.log("upload file status ", res.status, res.percent);
+              if (res.status === "progress") {
+                this.isLoading = true;
+                this.percentDone = res.percent;
+              }
+              if (res.status === undefined || res.status === "pre_process") {
+                this.fileUploaded = true;
+                this.isLoading = false;
+                this.uploadError = false;
+              }
+              return status;
+            },
+            (error: Error) => {
+              this.isDisabled = false;
+              this.isLoading = false;
+              this.uploadError = true;
+              return error;
+            }
+          );
+
+        // this.http
+        //   .post(
+        //     url,
+        //     {
+        //       DocumentType: "paystub",
+        //       Pages: [{ Data: this.base64File[1], Extension: fileExtension }],
+        //     },
+        //     {
+        //       reportProgress: true,
+        //       observe: "events",
+        //     }
+        //   )
+        //   .subscribe((event) => {
+        //     if (event.type === HttpEventType.UploadProgress) {
+        //       this.percentDone = Math.round((100 * event.loaded) / event.total);
+        //     } else if (event instanceof HttpResponse) {
+        //       this.fileUploaded = true;
+        //       console.log("event is an instance of a response", event.status);
+        //     }
+        //   });
+      };
+    } catch (error) {
+      this.uploadError = true;
+    }
   }
 }
--- src/app/file-upload/image-upload.service.ts
+++ src/app/file-upload/image-upload.service.ts
@@ -1,20 +1,113 @@
-import { map } from "rxjs/operators";
 import { Injectable } from "@angular/core";
-import { Response } from "@angular/http";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import {
+  HttpClient,
+  HttpEvent,
+  HttpEventType,
+  HttpErrorResponse,
+} from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
-@Injectable()
+interface IUploadImageResponse {
+  status: string;
+}
+
+interface IExistingFile {
+  id: string;
+  status: string;
+}
+
+interface IExistingFilesArray extends Array<IExistingFile> {}
+
+@Injectable({
+  providedIn: "root",
+})
 export class ImageUploadService {
-  constructor(private http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {}
+  percentDone: number;
+  uploadSuccess: boolean;
 
-  public uploadImage(image: File): Observable<string | any> {
-    const formData = new FormData();
+  private getEventMessage(event: HttpEvent<any>, fileUpload) {
+    switch (event.type) {
+      case HttpEventType.UploadProgress:
+        return this.fileUploadProgress(event);
+      case HttpEventType.Response:
+        return this.apiResponse(event);
+      default:
+        return `File "${fileUpload.DocumentType}" surprising upload event: ${event.type}.`;
+    }
+  }
 
-    formData.append("image", image);
+  private fileUploadProgress(event) {
+    const percentDone = Math.round((100 * event.loaded) / event.total);
+    return { status: "progress", percent: percentDone };
+  }
+
+  private apiResponse(event) {
+    return event.body;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error("ERROR:", error.error.message);
+    } else {
+      console.error(
+        `BACKEND ERROR CODE: ${error.status},` + `body was: ${error.error}`
+      );
+    }
+    return throwError("ERROR:", error.error);
+  }
+
+  uploadFile(image: string, type?: string): Observable<any> {
+    const fileUpload = {
+      DocumentType: "paystub",
+      Pages: [{ Data: image, Extension: type }],
+    };
+
+    console.log("uploading a file...", fileUpload);
+
+    const url = `https://torchcodelab.free.beeceptor.com`;
 
     return this.http
-      .post("/api/v1/image-upload", formData)
-      .pipe(map((json: any) => json.imageUrl));
+      .post<IUploadImageResponse>(url, fileUpload, {
+        reportProgress: true,
+        observe: "events",
+      })
+      .pipe(
+        map((event) => this.getEventMessage(event, fileUpload)),
+        catchError(this.handleError)
+      );
+  }
+
+  uploadImage(image: string, type?: string): Observable<string> {
+    const docUpload = {
+      DocumentType: "paystub",
+      Pages: [{ Data: image, Extension: type }],
+    };
+
+    const url = `https://torchcodelab.free.beeceptor.com`;
+
+    return this.http.post<IUploadImageResponse>(url, docUpload).pipe(
+      map((res: { status: string }) => res.status),
+      catchError((error: Error) => {
+        return throwError(error);
+      })
+    );
+
+    // this.http
+    //   .post(url, docUpload, {
+    //     reportProgress: true,
+    //     observe: "events",
+    //   })
+    //   .subscribe((event) => {
+    //     if (event.type === HttpEventType.UploadProgress) {
+    //       this.percentDone = Math.round((100 * event.loaded) / event.total);
+    //       return { status: "pre-process" };
+    //     } else if (event instanceof HttpResponse) {
+    //       this.uploadSuccess = true;
+    //       console.log("event is an instance of a response", event.status);
+    //       return event.status;
+    //     }
+    //   });
   }
 }
